4b77e13c18c37ef87243cedf002299a8992b632e
bmichalczuk <barmic90@hotmail.com>
2023-09-09 21:36:10 +0200

    xRevert "styled product list"
    This reverts commit aea7d51947afbcda51ecb1448de76b740290444e.

--- src/app/ui/atoms/ProductCoverImage.tsx
+++ src/app/ui/atoms/ProductCoverImage.tsx
@@ -11,7 +11,7 @@ export const ProductCoverImage = ({
 				width={320}
 				src={product.coverImage.src}
 				alt={product.coverImage.alt}
-				className="h-full w-full rounded-md border-2 border-gray-200 object-fill object-center p-4 transition hover:scale-105"
+				className="h-full w-full rounded-md border-2 border-gray-200 object-fill object-center p-4"
 			/>
 		</div>
 	);
